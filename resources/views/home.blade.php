72ed6083df024277902ace83378a6f49bcf3fc33
Wathsara <wvd.51461@gmail.com>
2018-10-03 23:22:39 +0530

    created the sample handling form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -760,9 +760,45 @@
                                 </div>
                                 {{--<canvas id="myChart" width="350" height="350"></canvas>--}}
                             </div>
+
+                            {{--tab of the smple handling--}}
                             <div class="tab-pane fade" id="sample" role="tabpanel" aria-labelledby="profile-tab">
                                 <div class="row">
-                                    sample handling
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                        Sample Handling
+                                    </button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form action="#" method="post">
+                                                        <label>Sample Number</label>
+                                                        <div class="form-group">
+                                                            <input type="text" class="form-control" name="sname" required>
+                                                        </div>
+                                                        <br>
+                                                        <label>Transfer Date</label>
+                                                        <div class="form-group">
+                                                            <input type="date" class="form-control" name="sname" required>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                                 {{--<canvas id="myChart" width="350" height="350"></canvas>--}}
                             </div>
